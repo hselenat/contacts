d5bd307490704e4490038d49c1bd6031e762b928
HSelenaT <942430793@qq.com>
2018-04-16 21:18:29 +0800

    使用react渲染UI

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import React, { Component } from 'react';
 
 class ContactList extends React.Component {
   render() {
-    const people = [
-      {name:'a'},
-      {name:'b'},
-      {name:'c'}
-    ]
+    const people = this.props.contacts 
     
     return <ol>
       {people.map(person =>(
@@ -21,12 +17,20 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <ContactList/>     
-        <ContactList/>
-        <ContactList/>
+        <ContactList contacts={[
+          {name:'a'},
+          {name:'b'},
+          {name:'c'}
+        ]}/>     
+        <ContactList contacts={[
+          {name:'d'},
+          {name:'e'},
+          {name:'f'}
+        ]}/>
       </div>
     );
   }
 }
 
 export default App;
+
